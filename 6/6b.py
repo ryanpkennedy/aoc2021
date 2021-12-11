7938b1a178303b430f5ee375ec8a1a5d8e24ea26
ryanpkennedy <ryan.kennedy@huskers.unl.edu>
2021-12-10 20:58:52 -0600

    im even better at code now

--- 6/6b.py
+++ 6/6b.py
@@ -4,37 +4,14 @@ list = [int(a) for a in list]
 
 days = 256
 
-dayTracker = {
-    0: 0,
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0, 
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-}
+dayTracker = [0 for x in range(9)]
 
 for fish in list: 
     dayTracker[fish] += 1
 
-def CountFish(): 
-    finalCount = 0
-    for day in dayTracker:
-        finalCount += dayTracker[day]
-    return finalCount
-
 for day in range(days):
-        newFish = dayTracker[0]
-        dayTracker[0] = dayTracker[1]
-        dayTracker[1] = dayTracker[2]
-        dayTracker[2] = dayTracker[3]
-        dayTracker[3] = dayTracker[4]
-        dayTracker[4] = dayTracker[5]
-        dayTracker[5] = dayTracker[6]
-        dayTracker[6] = dayTracker[7] + newFish
-        dayTracker[7] = dayTracker[8]
-        dayTracker[8] = newFish
+    newFish = dayTracker[0]
+    for x in range(len(dayTracker)):
+        dayTracker[x] =  (x < 8 and dayTracker[x + 1]) + ((x == 8 or x == 6) and newFish)
 
-print(CountFish())
+print(sum(dayTracker))
